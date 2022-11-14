84e7eecaba26d52d7ec916cf50444912e9f89022
MayumiNascimento <mayuminascimento9@gmail.com>
2022-11-14 12:56:39 -0300

    atualizando classes

--- src/telefonia/PrePago.java
+++ src/telefonia/PrePago.java
@@ -8,11 +8,12 @@ public class PrePago extends Assinante{
 
 	protected float credito;
 	protected int numRecargas;
-	protected Recarga[]recargas;
+	Recarga recargas[] = new Recarga[6];
+	
+	//protected Recarga[]recargas;
 	
 	
-	public PrePago(Date d, Integer duracao, String nome, int numero, long cpf, float credito, int numRecargas,
-			Recarga[] recargas) {
+	public PrePago(Date d, Integer duracao, String nome, int numero, long cpf, float credito, int numRecargas, Recarga[] recargas) {
 		super(d, duracao, nome, numero, cpf);
 		this.credito = credito;
 		this.numRecargas = numRecargas;
@@ -24,11 +25,21 @@ public class PrePago extends Assinante{
 	}
 	
 	public void recarregar(GregorianCalendar data, float valor) {
-		
+		boolean completo = true;
+		for(int i = 0; i < recargas.length; i++)
+			if (recargas[i] == null) {
+				
+			}
+			else {
+				System.out.println("não é possivel realizar mais recargas.");
+			}
 	}
 	public void imprimirFatura(int mes) {
 		
 	}
+		/*Assinante a;
+		a = new Assinante(21, 12, ant, 12, 382000);
+		a.getCpf();*/
 	
 
 
